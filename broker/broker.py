f0204bf96964ec403899c30ed535d413d597edc3
ccordi <chriscordi@gmail.com>
2016-02-23 05:39:45 +0000

    issue 6: added random secret

--- broker/broker.py
+++ broker/broker.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+import random 
+
 #open the file where connection information is stored (this users home directory)
 filename = os.path.expanduser('~') + "/connections.txt"
 if len(sys.argv) == 1:
@@ -11,8 +13,13 @@ elif sys.argv[1] == 'load':
         print fo.read()
 elif sys.argv[1] == 'save':
         cxn = sys.argv[2]
+        random.seed()
+        secret = random.getrandbits(2048)
+	job = str(cxn) + ":" + str(secret)
         fo = open(filename, "w")
-        fo.write(cxn)
+        fo.write(job)
+        print secret
+
 else:
         print( "invalid arg(s). Use 'load' or 'save' [cxn-info]")
 
