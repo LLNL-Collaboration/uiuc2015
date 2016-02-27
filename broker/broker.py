8fce9042ecbc3565d740f921d679e99bfe5a5898
ccordi <chriscordi@gmail.com>
2016-02-27 04:24:12 +0000

    issue19: added query to broker

--- broker/broker.py
+++ broker/broker.py
@@ -4,30 +4,90 @@ import sys
 import os
 import random 
 import binascii
+import json
+
+def is_json(myjson):
+        try:
+                json_object = json.loads(myjson)
+        except ValueError, e:
+                return False
+        return True
+
+def get_jobs():
+        filename = os.path.expanduser('~') + "/connections.txt"
+        fo = open(filename, "r")
+        jobs = fo.read().split("\n")
+        parsed_jobs = []
+        for job in jobs:
+                if not (is_json(job)):
+                        continue
+                parsed_jobs.append(json.loads(job))
+        return parsed_jobs
+
+def write_jobs(jobs):
+        fo = open(filename, "w")
+        fo.write(jobs)
+
+#test this
+def append_job(job):
+        with open(filename, "a") as fo:
+                fo.write(json.dumps(job))
+                fo.write("\n")
+
+def save_job():
+        port = get_fresh_port()
+        jobs = get_jobs()
+        # TODO
+        # check if job is already in jobs
+        # if it is, update the job, and rewrite file
+
+        # else just append new job
+        secret = binascii.hexlify(os.urandom(512))
+        job_id = "job_" + str(port)
+        job = {"job_id": job_id, "port": port, "secret": secret}
+        append_job(job)
+        return port
+
+def get_fresh_port():
+        jobs = get_jobs()
+        current_ports = set()
+        for job in jobs:
+                current_ports.add(job["port"])
+        new_port = random.randint(8000, 10000)
+        while new_port  in current_ports:
+                new_port = random.randint(8000, 10000)
+        return new_port
 
 #open the file where connection information is stored (this users home directory)
 filename = os.path.expanduser('~') + "/connections.txt"
 if len(sys.argv) == 1:
-        print( "invalid arg(s). Use 'load' or 'save [cxn-info]'")
+        print( "invalid arg(s). Use 'load [job-id]', 'query', or 'save [cxn-info]'")
 elif sys.argv[1] == 'load':
-        fo = open(filename, "r")
-        print fo.read()
+        job_id = sys.argv[2].strip()
+        jobs = get_jobs()
+        for job in jobs:
+                if job["job_id"] == job_id:
+                        print(json.dumps(job))
+
+elif sys.argv[1] == 'query':
+        jobs = get_jobs()
+        for job in jobs:
+                print job["job_id"]
+
 elif sys.argv[1] == 'verify':
-        fo = open(filename, "r")
-        real_secret = fo.read().split(":")[1].strip()
-        client_secret = sys.argv[2].strip()
-        valid = real_secret == client_secret
+        job_id = sys.argv[2].strip()
+        secret = sys.argv[3].strip()
+        valid = False
+        jobs = get_jobs()
+        for job in jobs:
+                if job["job_id"] == job_id:
+                        if job["secret"] == secret:
+                                valid = True
         print valid
-        # print "actual secret is:" + real_secret
-        # print "client secret is:" + client_secret
-        # print "they are the same: " + str(real_secret == client_secret)
+
 elif sys.argv[1] == 'save':
-        cxn = sys.argv[2]
-        secret = binascii.hexlify(os.urandom(512))
-        job = cxn +  ":" + str(secret) 
-        fo = open(filename, "w")
-        fo.write(job)
-        print secret
+        port = save_job()
+        print(port)
 else:
-        print( "invalid arg(s). Use 'load' or 'save' [cxn-info]")
+        print( "invalid arg(s). Use 'load [job-id]', 'query', or 'save [cxn-info]'")
 
