065abc0ed42c1983dca24254380ee4766f770a40
Bhaavya <bhavya2@illinois.edu>
2016-03-09 04:20:21 +0000

    issue17:Broker scripts updated for ssl case

---
+++ broker/certgen.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [[ ! $# == 1 ]]; then
+ echo error
+ exit
+fi
+
+FILEN=$1
+PASS_FILE="pass/pass.txt"
+{
+openssl genrsa -des3 -passout file:${PASS_FILE} -out ${FILEN}.key 1024 
+openssl req -new -key ${FILEN}.key -passin file:${PASS_FILE} -out ${FILEN}.csr -subj '/O=Conduit'
+cp ${FILEN}.key ${FILEN}.key.orig 
+openssl rsa -in ${FILEN}.key.orig -passin file:${PASS_FILE} -out ${FILEN}.key
+openssl x509 -req -days 3650 -in ${FILEN}.csr -signkey ${FILEN}.key -out ${FILEN}.crt 
+cp ${FILEN}.crt ${FILEN}.pem
+cat ${FILEN}.key >> ${FILEN}.pem
+} &> ${FILEN}.log
