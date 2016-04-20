e7977ef7f8830ee8b6fcff2eebcfbfa70eac5076
Donggun Kim <dkim91@gmail.com>
2016-04-20 12:34:20 -0500

    refactoring client.py and helpers.py

--- broker/client.py
+++ broker/client.py
@@ -8,56 +8,56 @@ from config import *
 from helpers import run
 
 def query(host=BROKER_IP):
-	command = BROKER_PATH + ' query'
-	ret = run(command, host)[0]
-	return ret
+    command = BROKER_PATH + ' query'
+    ret = run(command, host)[0]
+    return ret
 
 def load(job_id, host=BROKER_IP):
-	command = BROKER_PATH +' load ' + job_id
-	ret = run(command, host)
-	if len(ret) == 0:
-		print "Job not found"
-		exit()
-	else:
-		ret = ret[0]
-	job = json.loads(ret)
-	job_id = job["job_id"]
-	port = str(job["port"])
-	secret = job.get("secret")
-	ssl = job.get('ctype')
-	#print("Broker says the job_id is: " + job_id)
-	#print("Broker says the port is: " + port)
-	#print("Broker says the secret is: " + secret)
-	return (job_id, port, secret, ssl)
+    command = BROKER_PATH +' load ' + job_id
+    ret = run(command, host)
+    if len(ret) == 0:
+        print "Job not found"
+        exit()
+    else:
+        ret = ret[0]
+    job = json.loads(ret)
+    job_id = job["job_id"]
+    port = str(job["port"])
+    secret = job.get("secret")
+    ssl = job.get('ctype')
+    #print("Broker says the job_id is: " + job_id)
+    #print("Broker says the port is: " + port)
+    #print("Broker says the secret is: " + secret)
+    return (job_id, port, secret, ssl)
 
 def fetch_job(job_id, host="localhost"):
-	job_id, port, secret, ssl = load(job_id, BROKER_IP)
-	if ssl == 'ssl':
-		print "Connect via web browser to: '" + host +"' on port: '" + port + "'"
-		return
-	tunnel = "ssh -fnNT -L 6005:" + host + ":" + port + " " + USERNAME + "@" + host
-	#print("Opening a tunnel: \n" + tunnel)
-	os.system(tunnel)
-	command = 'echo "' + secret + '" | nc localhost 6005'
-	#print("Connecting to server with the secret: " + command)
-	os.system(command)
+    job_id, port, secret, ssl = load(job_id, BROKER_IP)
+    if ssl == 'ssl':
+        print "Connect via web browser to: '" + host +"' on port: '" + port + "'"
+        return
+    tunnel = "ssh -fnNT -L 6005:" + host + ":" + port + " " + USERNAME + "@" + host
+    #print("Opening a tunnel: \n" + tunnel)
+    os.system(tunnel)
+    command = 'echo "' + secret + '" | nc localhost 6005'
+    #print("Connecting to server with the secret: " + command)
+    os.system(command)
 
 if __name__ == "__main__":
-	USERNAME = getpass.getuser()
+    USERNAME = getpass.getuser()
 
-	parser = optparse.OptionParser()
-	parser.add_option('-l', '--load', dest='load',
-	                  help='returns job information about job_id')
-	parser.add_option('-q', '--query', action='store_true',
-	                  dest='query', help='returns active job names from broker script')
-	options, args = parser.parse_args()
+    parser = optparse.OptionParser()
+    parser.add_option('-l', '--load', dest='load',
+                      help='returns job information about job_id')
+    parser.add_option('-q', '--query', action='store_true',
+                      dest='query', help='returns active job names from broker script')
+    options, args = parser.parse_args()
 
-	if options.load:
-		fetch_job(options.load, SERVER_IP)
+    if options.load:
+        fetch_job(options.load, SERVER_IP)
 
-	elif options.query:
-		jobs = query(BROKER_IP)
-		print jobs
+    elif options.query:
+        jobs = query(BROKER_IP)
+        print jobs
 
-	else:
-		print "invalid arg(s). Use 'query' or 'load [job_id]'"
+    else:
+        print "invalid arg(s). Use 'query' or 'load [job_id]'"
--- broker/helpers.py
+++ broker/helpers.py
@@ -1,38 +1,35 @@
 
-from pexpect import pxssh
 import getpass
 import os
 import sys
 import json
-from config import *
 
 import subprocess
-
-
-
+from pexpect import pxssh
+from config import *
 
 def run(command, host):
-	if LOCAL:
-		output = subprocess.check_output(command, shell=True)
-		return [output]
+    if LOCAL:
+        output = subprocess.check_output(command, shell=True)
+        return [output]
 
-	try:
-		s = pxssh.pxssh()
-		hostname = host
-		username = getpass.getuser()
-		s.login (hostname, username)
-		s.sendline (command)
-		s.prompt()
-		output = s.before.split("\r\n")[1:-1]
-		s.logout()
-		return output
-	except pxssh.ExceptionPxssh,e:
-	        print "pxssh failed on login."
-	        print str(e)
+    try:
+        s = pxssh.pxssh()
+        hostname = host
+        username = getpass.getuser()
+        s.login(hostname, username)
+        s.sendline(command)
+        s.prompt()
+        output = s.before.split("\r\n")[1:-1]
+        s.logout()
+        return output
+    except pxssh.ExceptionPxssh, e:
+        print "pxssh failed on login."
+        print str(e)
 
 def is_json(myjson):
-        try:
-                json_object = json.loads(myjson)
-        except ValueError, e:
-                return False
-        return True
+    try:
+        json_object = json.loads(myjson)
+    except ValueError, e:
+        return False
+    return True
